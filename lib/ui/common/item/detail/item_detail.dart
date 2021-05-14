5022b1dd64b09f7537af4adc99ee5b7b4cb42c1b
Sanjib <sanjib.maharjan@debajyotigroup.com>
2021-05-14 23:31:16 +0545

    make details page responsive

--- lib/ui/common/item/detail/item_detail.dart
+++ lib/ui/common/item/detail/item_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_shopping_app/constant/color.dart';
+import 'package:flutter_shopping_app/ui/common/base/app_wrapper.dart';
 import 'package:flutter_shopping_app/ui/common/form/custom_button.dart';
 import 'package:flutter_shopping_app/ui/common/incrementer.dart';
 import 'package:flutter_shopping_app/ui/common/item/data/model/product_item.dart';
@@ -28,91 +29,98 @@ class _ItemDetailState extends State<ItemDetail> {
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
-          body: Stack(
+          body: Column(
             children: [
-              ListView(
-                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton.icon(
-                        style: ButtonStyle(
-                            foregroundColor:
-                                MaterialStateProperty.all(ThemeColor)),
-                        onPressed: () => _goBack(context),
-                        icon: Icon(Icons.keyboard_arrow_left),
-                        label: Text("Back")),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Image.network(
-                    widget.item.imageUrl,
-                    height: 300,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    widget.item.name,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  RatingBar.builder(
-                    initialRating: min(widget.item.rating ?? 0, 5),
-                    minRating: 1,
-                    itemSize: 20,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    itemBuilder: (context, _) =>
-                        Icon(Icons.star, color: Colors.amber),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Incrementer(
-                        setIncrementer: _updateCartCount,
-                        value: _noOfItems,
+              Expanded(
+                child: SingleChildScrollView(
+                  child: AppWrapper(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: TextButton.icon(
+                                style: ButtonStyle(
+                                    foregroundColor:
+                                        MaterialStateProperty.all(ThemeColor)),
+                                onPressed: () => _goBack(context),
+                                icon: Icon(Icons.keyboard_arrow_left),
+                                label: Text("Back")),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Center(
+                            child: Image.network(
+                              widget.item.imageUrl,
+                              height: 300,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            widget.item.name,
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          RatingBar.builder(
+                            initialRating: min(widget.item.rating ?? 0, 5),
+                            minRating: 1,
+                            itemSize: 20,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemBuilder: (context, _) =>
+                                Icon(Icons.star, color: Colors.amber),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Incrementer(
+                                setIncrementer: _updateCartCount,
+                                value: _noOfItems,
+                              ),
+                              Text(
+                                widget.item.price,
+                                style: TextStyle(fontSize: 30),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text("About the  Product",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            widget.item.description ?? "",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: ThemeTextColor,
+                            ),
+                            textAlign: TextAlign.justify,
+                          ),
+                          if (widget.item.hasBenefits) _addItemBenefits(),
+
+                        ],
                       ),
-                      Text(
-                        widget.item.price,
-                        style: TextStyle(fontSize: 30),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text("About the  Product",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    widget.item.description ?? "",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: ThemeTextColor,
                     ),
-                    textAlign: TextAlign.justify,
                   ),
-                  if (widget.item.hasBenefits) _addItemBenefits(),
-                  SizedBox(
-                    height: 70,
-                  )
-                ],
+                ),
               ),
-              Align(
-                child: _addToCartWidget(),
-                alignment: Alignment.bottomCenter,
-              )
+              _addToCartWidget()
             ],
           ),
         ),
@@ -122,27 +130,31 @@ class _ItemDetailState extends State<ItemDetail> {
 
   Widget _addToCartWidget() {
     return Container(
+      // alignment: Alignment.bottomCenter,
+      width: double.infinity,
       decoration: BoxDecoration(
           color: Colors.white,
           border: Border(
               top: BorderSide(width: 1.0, color: ThemeTextColorLighter))),
       padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            icon: Icon(Icons.favorite_border),
-            onPressed: _addToFavourite,
-          ),
-          CustomButton(
-            label: "Add to cart",
-            padding: EdgeInsets.symmetric(
-              vertical: 15,
-              horizontal: 60,
+      child: AppWrapper(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.favorite_border),
+              onPressed: _addToFavourite,
             ),
-            onPressed: _addToCart,
-          ),
-        ],
+            CustomButton(
+              label: "Add to cart",
+              padding: EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 60,
+              ),
+              onPressed: _addToCart,
+            ),
+          ],
+        ),
       ),
     );
   }
