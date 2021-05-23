ebea457296ad0ecb4e213c0dcd602153e27f3b39
Sanjib <sanjib.maharjan@debajyotigroup.com>
2021-05-23 11:50:49 +0545

    added a generic paginate class

---
+++ lib/data/models/paginate.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+abstract class Paginate<T> {
+  static const DEFAULT_ITEM_PER_PAGE = 10;
+
+  final List<T> data;
+
+  @JsonKey(name: 'has_next')
+  final bool hasNext;
+
+  @JsonKey(name: 'has_prev')
+  final bool hasPrev;
+
+  final int page;
+
+  @JsonKey(name: 'total_pages')
+  final int totalPages;
+
+  Paginate({this.data, this.hasNext, this.hasPrev, this.page, this.totalPages});
+}
